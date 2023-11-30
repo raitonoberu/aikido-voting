c7b805a413acbfa57ce8489e2fd102dfb12331cc
raitonoberu <raitonoberu@mail.ru>
2023-11-30 19:33:26 +0500

    Fix CORS

--- backend/main.go
+++ backend/main.go
@@ -21,6 +21,7 @@ func main() {
 	// api
 	r := gin.Default()
 	api := r.Group("/api")
+	api.Use(CORSMiddleware)
 
 	// auth
 	auth := new(controllers.AuthController)
@@ -41,4 +42,18 @@ func main() {
 	api.DELETE("/pool/:id", auth.Middleware, pool.Delete)
 
 	panic(r.Run(":8080"))
+}
+
+func CORSMiddleware(c *gin.Context) {
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET, POST, UPDATE, DELETE")
+
+	if c.Request.Method == "OPTIONS" {
+		c.AbortWithStatus(204)
+		return
+	}
+
+	c.Next()
 }
